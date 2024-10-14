49ab4ce8b0b173766822319264a8d5bdf8676b4f
melihHadzhimurad <melihchohacimurat10@gmail.com>
2024-10-14 19:38:53 +0300

    Preparing the data to visualisation

--- data_visualisation.py
+++ data_visualisation.py
@@ -3,5 +3,21 @@ import matplotlib.pyplot as plot
 
 data_frame = data_processing.get_data_in_frame()
 
-print(data_frame)
+# Average grade for each year
+# print(data_frame.groupby("Year")["Grade"].mean())
 
+# Average grade for each semester
+# print(data_frame.groupby("Semester")["Grade"].mean())
+
+# number of each grade as the student has
+"""
+print("Twos: \n" + str(data_frame[data_frame["Grade"] == 2]["Subject"].count()))
+print("Threes: \n" + str(data_frame[data_frame["Grade"] == 3]["Subject"].count()))
+print("Fours: \n" + str(data_frame[data_frame["Grade"] == 4]["Subject"].count()))
+print("Fives: \n" + str(data_frame[data_frame["Grade"] == 5]["Subject"].count()))
+print("Sixes: \n" + str(data_frame[data_frame["Grade"] == 6]["Subject"].count()))
+"""
+
+# print(data_frame.groupby("Session")["Subject"].count())
+
+print(data_frame)
